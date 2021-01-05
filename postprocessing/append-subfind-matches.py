55e59ca6362244742e5560c4fa4a72bfe4ca5867
Austen Gabrielpillai <a.gabrielpillai@gmail.com>
2021-01-05 16:59:10 -0500

    fixes to creating match.hdf5 from popeye

--- postprocessing/append-subfind-matches.py
+++ postprocessing/append-subfind-matches.py
@@ -1,9 +1,9 @@
 import sys
 import site
 
-sys.path.append('/mnt/home/agabrielpillai/.local/lib/python3.6/site-packages/')
-site.addsitedir('/mnt/home/agabrielpillai/') 
-site.addsitedir('/mnt/home/agabrielpillai/scripts/') 
+sys.path.append('/home/agabrielpillai/.local/lib/python3.6/site-packages/')
+site.addsitedir('/home/agabrielpillai/') 
+site.addsitedir('/home/agabrielpillai/scripts/') 
 
 import h5py
 
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 basePathSAM = str(sys.argv[1])
 matchPath = str(sys.argv[2])
 snap_start = int(sys.argv[3])
+nsubvol = int(sys.argv[4])
 
 matches_DM = h5py.File('{}/matches_DM.hdf5'.format(matchPath), 'r')
 matches_FP = h5py.File('{}/matches_FP.hdf5'.format(matchPath), 'r')
@@ -24,12 +25,12 @@ matches_FP = h5py.File('{}/matches_FP.hdf5'.format(matchPath), 'r')
 offset = np.zeros(100)
 subvolumes = []
 
-for i in range(5):
-    for j in range(5):
-        for k in range(5):
+for i in range(nsubvol):
+    for j in range(nsubvol):
+        for k in range(nsubvol):
             subvolumes.append([i, j, k])
 
-#subvolumes.append([0, 0, 0])
+# subvolumes.append([3, 3, 5])
 
 for subvolume in subvolumes: 
     head = ilsam.groupcat.load_header(basePathSAM, subvolume)
@@ -45,6 +46,7 @@ for subvolume in subvolumes:
     
     for i in range(snap_start, 100):
         # get the values in haloprop that are in that snapshot, then pull those out of the match files and assign
+        print(i, haloprop_SAM.loc[haloprop_SAM['snap_num'] == i, 'subfind-idx-DM'].shape, matches_DM['Subfind'][str(i)][int(offset[i]):int(offset[i]+head['Ngroups_ThisFile_Redshift'][i])][:].shape)
         haloprop_SAM.loc[haloprop_SAM['snap_num'] == i, 'subfind-idx-DM'] = \
             matches_DM['Subfind'][str(i)][int(offset[i]):int(offset[i]+head['Ngroups_ThisFile_Redshift'][i])][:]
         haloprop_SAM.loc[haloprop_SAM['snap_num'] == i, 'fof-idx-DM'] = \
@@ -72,15 +74,15 @@ for subvolume in subvolumes:
     f = h5py.File('{}/output/{}_{}_{}/matches.hdf5'.format(basePathSAM, *subvolume), 'w')
 
     subhalos = f.create_group("Galprop")
-    subhalos_FP = subhalos.create_dataset('GalpropSubfindIndex_FP', (galprop_SAM.shape[0], ), dtype='int32',
+    subhalos_FP = subhalos.create_dataset('GalpropSubfindIndex_DM', (galprop_SAM.shape[0], ), dtype='int32',
                                           data=galprop_SAM['subfind-idx-FP'])
-    subhalos_DM = subhalos.create_dataset('GalpropSubfindIndex_DM', (galprop_SAM.shape[0], ), dtype='int32',
+    subhalos_DM = subhalos.create_dataset('GalpropSubfindIndex_FP', (galprop_SAM.shape[0], ), dtype='int32',
                                           data=galprop_SAM['subfind-idx-DM'])
 
     halos = f.create_group("Haloprop")
-    halos_FP = halos.create_dataset('HalopropFoFIndex_FP', (haloprop_SAM.shape[0], ), dtype='int32',
+    halos_FP = halos.create_dataset('HalopropFoFIndex_DM', (haloprop_SAM.shape[0], ), dtype='int32',
                                        data=haloprop_SAM['fof-idx-FP'])
-    halos_DM = halos.create_dataset('HalopropFoFIndex_DM', (haloprop_SAM.shape[0], ), dtype='int32',
+    halos_DM = halos.create_dataset('HalopropFoFIndex_FP', (haloprop_SAM.shape[0], ), dtype='int32',
                                           data=haloprop_SAM['fof-idx-DM'])
 
     f.close()
--- postprocessing/snap-SAM-match.py
+++ postprocessing/snap-SAM-match.py
@@ -5,9 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 
-sys.path.append('/mnt/home/agabrielpillai/.local/lib/python3.6/site-packages/')
-site.addsitedir('/mnt/home/agabrielpillai/') 
-site.addsitedir('/mnt/home/agabrielpillai/scripts/')
+sys.path.append('/home/agabrielpillai/.local/lib/python3.6/site-packages/')
+site.addsitedir('/home/agabrielpillai/') 
+site.addsitedir('/home/agabrielpillai/scripts/')
 
 import illustris_python as il
 import illustris_sam as ilsam
@@ -16,15 +16,29 @@ basePathTNG = str(sys.argv[1])
 basePathSAM = str(sys.argv[2])
 basePathTNG_DM = str(sys.argv[3])
 basePathTNG_RS = str(sys.argv[4])
-matchOutPath = str(sys.argv[5])
-imgPath = str(sys.argv[6])
-snapStart = int(sys.argv[7])
+match_file_path= str(sys.argv[5]) #'/mnt/home/agabrielpillai/ceph/IllustrisTNG/L75n1820TNG_DM/output/postprocessing/rockstar/matching'
 
+matchOutPath = str(sys.argv[6])
+imgPath = str(sys.argv[7])
+
+snapStart = int(sys.argv[8])
+nsubvol = int(sys.argv[9])
+
+DMO_match = h5py.File('{}/rockstar_subhalo_matching_to_DM.hdf5'.format(match_file_path), 'r')  
+FP_match = h5py.File('{}/rockstar_subhalo_matching_to_FP.hdf5'.format(match_file_path), 'r')
+
+f_FP = h5py.File(matchOutPath + '/matches_FP.hdf5', 'w')
+FP_G = f_FP.create_group("Subfind")
+FP_H = f_FP.create_group("FoF")
+
+f_DM = h5py.File(matchOutPath + '/matches_DM.hdf5', 'w')
+DM_G = f_DM.create_group("Subfind")
+DM_H = f_DM.create_group("FoF")
 
 def genParams(ax, func, bounds, sam, tng):
     params_1 = func(ax, bounds, sam, tng, 2)
     f = drawHeatmap(ax, params_1[0], params_1[1], params_1[2], 'gray')
-    return (params_1)
+    return(params_1)
 
 
 def drawHeatmap(ax, heatmap, extent, bounds, col_map):
@@ -34,11 +48,11 @@ def drawHeatmap(ax, heatmap, extent, bounds, col_map):
     f = ax.imshow(heatmap, extent=extent, origin='lower', aspect='auto', cmap=col_map, interpolation='nearest')
     cb = plt.colorbar(f, ax=ax)
     cb.set_label('$log_{10}counts$')
-    return (heatmap)
+    return(heatmap)
 
 
 def genHeatmap(ax, q1, q2, bounds):
-    heatmap, xedges, yedges = np.histogram2d(q1, q2, range=bounds, bins=(200, 200))
+    heatmap, xedges, yedges = np.histogram2d(q1, q2, range=bounds, bins = (200, 200))
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
     heatmap = np.log10(heatmap.T)
     return [heatmap, extent, bounds]
@@ -48,7 +62,7 @@ def m200_compare_2(ax, bounds, sam, tng, n):
     th = bounds[0][0]
 
     s = np.log10(sam['mhalo'] * 1e9)
-    t = np.log10(1e10 * tng['mhalo'] / 0.6674)
+    t = np.log10(1e10*tng['mhalo'] / 0.6674)
 
     dif = t - s
 
@@ -60,45 +74,26 @@ def genTNGDF(basePath, snapNum):
     group = il.groupcat.loadHalos(basePath, snapNum, fields=['Group_M_TopHat200', 'GroupFirstSub', 'Group_R_TopHat200'])
 
     tng_hydro = pd.DataFrame()
-    tng_hydro['x'] = subhalos['SubhaloPos'][:, 0]
-    tng_hydro['y'] = subhalos['SubhaloPos'][:, 1]
-    tng_hydro['z'] = subhalos['SubhaloPos'][:, 2]
+    tng_hydro['x'] = subhalos['SubhaloPos'][:,0]
+    tng_hydro['y'] = subhalos['SubhaloPos'][:,1]
+    tng_hydro['z'] = subhalos['SubhaloPos'][:,2]
     tng_hydro['host_idx'] = subhalos['SubhaloGrNr']
     tng_hydro['mhalo'] = group['Group_M_TopHat200'][subhalos['SubhaloGrNr']]
     tng_hydro['rhalo'] = group['Group_R_TopHat200'][subhalos['SubhaloGrNr']]
     tng_hydro = tng_hydro.reset_index(drop=False)
     tng_hydro['central'] = False
     tng_hydro['central'].iloc[group['GroupFirstSub'][group['GroupFirstSub'] != -1]] = True
-
+    
     return tng_hydro
 
 
-def genMatches(match_type, snapNum, rockstar, tng):
+def genMatches(match_file, snapNum, rockstar, tng):
     # will later change to just the full file ...
-    match = h5py.File(
-        basePathTNG_RS + '/postprocessing/rockstar/matching/DM-' + match_type + '/dm_match_%03d.hdf5' % snapNum, 'r')
     match_df = pd.DataFrame()
-    match_df['rockstar-idx'] = match['DescendantIndex']
+    match_df['rockstar-idx'] = match_file['Snapshot_{}'.format(snapNum)]['SubhaloIndexDarkRockstar_SubLink'][:]
     match_df = match_df.reset_index(drop=False)
-
-    # will later change to just the full file ...
-    match_rev = h5py.File(
-        basePathTNG_RS + '/postprocessing/rockstar/matching/DM-' + match_type + '/dm_match_reverse_%03d.hdf5' % snapNum,
-        'r')
-    rev_df = pd.DataFrame()
-    rev_df['subfind-idx'] = match_rev['DescendantIndex']
-    rev_df = rev_df.reset_index(drop=False)
-
-    # remove any potential out of bound matches if they exist
-    match_df.loc[match_df['rockstar-idx'] > rev_df.shape[0], 'rockstar-idx'] = -1
-    rev_df.loc[rev_df['subfind-idx'] > match_df.shape[0], 'subfind-idx'] = -1
-
-    match_df['from-rs-idx'] = rev_df.iloc[match_df['rockstar-idx']]['subfind-idx'].values
-    match_df['bijective'] = match_df['index'] == match_df['from-rs-idx']
-    match_df.loc[~match_df['bijective'], 'rockstar-idx'] = -1
-    match_df = match_df.drop(['from-rs-idx', 'bijective'], axis=1)
     match_df = match_df.rename(columns={"index": "subfind-idx"})
-
+                          
     match_df = match_df[match_df['rockstar-idx'] != -1].reset_index(drop=True)
     match_df['Rockstar-ID'] = rockstar.iloc[match_df['rockstar-idx']]['Subhalo_ID'].values
     match_df['tng-central'] = tng.iloc[match_df['subfind-idx']]['central'].values
@@ -106,13 +101,13 @@ def genMatches(match_type, snapNum, rockstar, tng):
     match_df = match_df[match_df['tng-central']].reset_index(drop=True).drop(['tng-central'], axis=1)
     match_df['haloprop-idx'] = rockstar.iloc[match_df['rockstar-idx']]['haloprop-idx'].values
     match_df = match_df[match_df['haloprop-idx'] != -1].reset_index(drop=True)
-
+                          
     return match_df
 
-
+                          
 def appendMatches(match_type, haloprop, tng, matches):
-    haloprop['subfind_id'] = -1
-    haloprop['fof_id'] = -1
+    haloprop['subfind_id'] = -1 
+    haloprop['fof_id'] = -1 
     haloprop.loc[matches['haloprop-idx'], 'subfind_id'] = matches['subfind-idx'].values
     haloprop.loc[matches['haloprop-idx'], 'fof_id'] = matches['tng-host-idx'].values
 
@@ -124,22 +119,14 @@ def appendMatches(match_type, haloprop, tng, matches):
     fig, ax = plt.subplots(1, 1, figsize=(8, 8))
     bounds = [[8, 15], [-1.5, 1.5]]
     maps = genParams(ax, m200_compare_2, bounds, SAM_test, tng_sub)
-    # fig.show()
+    #fig.show()
 
     ax.set_xlabel('$log_{10} m_{vir, SAM}$ $[M_{\odot}$]')
-    ax.set_ylabel('$log_{10} m_{200, TNG (' + match_type + ')} - log_{10} m_{vir, SAM}$ $[M_{\odot}]$')
-    plt.savefig(imgPath + '/' + match_type + '/' + str(snapNum) + '.png', dpi=300)
+    ax.set_ylabel('$log_{10} m_{200, TNG ('+match_type+')} - log_{10} m_{vir, SAM}$ $[M_{\odot}]$')
+    plt.savefig(imgPath + '/'+ match_type + '/' + str(snapNum) + '.png', dpi = 300)
 
     return haloprop
 
-
-f_FP = h5py.File(matchOutPath + '/matches_FP.hdf5', 'w')
-FP_G = f_FP.create_group("Subfind")
-FP_H = f_FP.create_group("FoF")
-
-f_DM = h5py.File(matchOutPath + '/matches_DM.hdf5', 'w')
-DM_G = f_DM.create_group("Subfind")
-DM_H = f_DM.create_group("FoF")
 
 for snapNum in range(snapStart, 100):
     tng_hydro = genTNGDF(basePathTNG, snapNum)
@@ -147,9 +134,9 @@ for snapNum in range(snapStart, 100):
 
     subvolume_list = []
 
-    for i in range(5):
-        for j in range(5):
-            for k in range(5):
+    for i in range(nsubvol):
+        for j in range(nsubvol):
+            for k in range(nsubvol):
                 subvolume_list.append([i, j, k])
 
     # subvolume_list = [[0, 0, 0]]
@@ -177,14 +164,14 @@ for snapNum in range(snapStart, 100):
     rockstar_df.loc[SAM_sub['orig_halo_ID'], 'haloprop-idx'] = SAM_sub['halo_index'].values
     rockstar_df = rockstar_df.sort_values(by='index').reset_index(drop=True)
 
-    match_df_DM = genMatches('DM', snapNum, rockstar_df, tng_hydro_DM)
+    match_df_DM = genMatches(DMO_match, snapNum, rockstar_df, tng_hydro_DM)
     SAM_haloprop_DM = appendMatches('DM', SAM_haloprop, tng_hydro_DM, match_df_DM)
     match_snap_DM_1 = FP_G.create_dataset(str(snapNum), (SAM_haloprop_DM.shape[0],),
                                           dtype='int32', data=SAM_haloprop_DM['subfind_id'].values.astype(float))
     match_snap_DM_2 = FP_H.create_dataset(str(snapNum), (SAM_haloprop_DM.shape[0],),
                                           dtype='int32', data=SAM_haloprop_DM['fof_id'].values.astype(float))
 
-    match_df_FP = genMatches('FP', snapNum, rockstar_df, tng_hydro)
+    match_df_FP = genMatches(FP_match, snapNum, rockstar_df, tng_hydro)
     SAM_haloprop_FP = appendMatches('FP', SAM_haloprop, tng_hydro, match_df_FP)
     match_snap_FP_1 = DM_G.create_dataset(str(snapNum), (SAM_haloprop_FP.shape[0],),
                                           dtype='int32', data=SAM_haloprop_FP['subfind_id'].values.astype(float))
